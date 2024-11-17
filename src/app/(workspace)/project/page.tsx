4f4c68149e2f72effa25ea36dbf9b7d0755576e0
Arnav-03 <arnavarora0003@gmail.com>
2024-11-17 20:15:41 +0530

    create project started

---
+++ src/app/(workspace)/project/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React, { useState } from "react";
+import Layout from "@/components/website/Layout";
+
+
+
+const page: React.FC = () => {
+
+  return (
+    <Layout>
+     <div className=""></div>
+    </Layout>
+  );
+};
+
+export default page;
